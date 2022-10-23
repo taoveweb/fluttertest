f01d095e2a75fcb3d9f2096772dd958aa09bcd55
淘未 <taowei@taoweideMacBook-Pro.local>
2022-10-23 17:06:27 +0800

    feat:拖动文本

--- lib/common/init_binding.dart
+++ lib/common/init_binding.dart
@@ -1,6 +1,9 @@
+import 'package:fluttertest/pages/publish/publish_controller.dart';
 import 'package:get/instance_manager.dart';
 
 class InitBinding extends Bindings {
   @override
-  void dependencies() {}
+  void dependencies() {
+    Get.put(PublishController());
+  }
 }
--- lib/common/widgets/flutter_photo_filter.dart
+++ lib/common/widgets/flutter_photo_filter.dart
@@ -1,13 +1,20 @@
 import 'dart:async';
+import 'dart:ffi';
 import 'dart:io';
 
+import 'package:draggable_fab/draggable_fab.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fluttertest/common/utils/util.dart';
+import 'package:fluttertest/common/widgets/floating_draggable_widget.dart';
+import 'package:fluttertest/common/widgets/text_page.dart';
+import 'package:fluttertest/pages/publish/publish_controller.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:image/image.dart' as imageLib;
 import 'package:path_provider/path_provider.dart';
 import 'package:photofilters/filters/filters.dart';
+import 'package:draggable_widget/draggable_widget.dart';
 
 class FluttertestPhotoFilter extends StatelessWidget {
   final imageLib.Image image;
@@ -87,6 +94,7 @@ class _fluttertestPhotoFilterSelectorState
   Filter? _filter;
   imageLib.Image? image;
   late bool loading;
+  final dragController = DragController();
 
   @override
   void initState() {
@@ -102,122 +110,200 @@ class _fluttertestPhotoFilterSelectorState
     super.dispose();
   }
 
+  final GlobalKey globalKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
-    var positionedBigImage = Positioned(
-      top: 0,
-      bottom: 0,
-      right: 0,
-      left: 0,
-      child: _buildFilteredImage(
-        _filter,
-        image,
-        filename,
-      ),
-    );
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          elevation: 0,
-          title: widget.title,
-          backgroundColor: widget.appBarColor,
-          actions: <Widget>[
-            loading
-                ? Container()
-                : GestureDetector(
-                    onTap: () async {
-                      setState(() {
-                        loading = true;
-                      });
-                      var imageFile = await saveFilteredImage();
+    positionedBigImage(PublishController conroller) {
+      return Positioned(
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0,
+        child: _buildFilteredImage(_filter, image, filename, conroller),
+      );
+    }
 
-                      Navigator.pop(context, {'image_filtered': imageFile});
-                    },
-                    child: Padding(
-                      padding: EdgeInsets.only(top: 20.w, right: 28.w),
-                      child: GradientText(
-                        '发布',
-                        textAlign: TextAlign.left,
-                        gradient: const LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [
-                              Color.fromRGBO(80, 253, 246, 1),
-                              Color.fromRGBO(56, 186, 255, 1)
-                            ]),
-                        style: TextStyle(
-                          fontSize: 16.sp,
+    var safeArea = SafeArea(
+      child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            elevation: 0,
+            title: widget.title,
+            backgroundColor: widget.appBarColor,
+            actions: <Widget>[
+              loading
+                  ? Container()
+                  : GestureDetector(
+                      onTap: () async {
+                        setState(() {
+                          loading = true;
+                        });
+                        var imageFile = await saveFilteredImage();
+
+                        Navigator.pop(context, {'image_filtered': imageFile});
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.only(top: 20.w, right: 28.w),
+                        child: GradientText(
+                          '发布',
+                          textAlign: TextAlign.left,
+                          gradient: const LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                Color.fromRGBO(80, 253, 246, 1),
+                                Color.fromRGBO(56, 186, 255, 1)
+                              ]),
+                          style: TextStyle(
+                            fontSize: 16.sp,
+                          ),
                         ),
                       ),
-                    ),
-                  )
-          ],
-        ),
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Colors.white,
-          child: loading
-              ? widget.loader
-              : Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.w),
-                        child: Container(
-                            width: double.infinity,
-                            height: double.infinity,
-                            margin: EdgeInsets.symmetric(
-                                horizontal: 41.w, vertical: 30.w),
-                            child: Stack(
-                              children: [
-                                positionedBigImage,
-                              ],
-                            )),
-                      ),
-                    ),
-                    Container(
-                      height: 118.w,
-                      color: Colors.white,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 41.w, vertical: 0.w),
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: widget.filters.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return InkWell(
-                            child: Container(
-                              color: Colors.white,
-                              padding: const EdgeInsets.all(5.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: <Widget>[
-                                  _buildFilterThumbnail(
-                                      widget.filters[index], image, filename),
-                                  const SizedBox(
-                                    height: 5.0,
-                                  ),
-                                  Text(
-                                    widget.filters[index].name,
-                                  )
-                                ],
-                              ),
-                            ),
-                            onTap: () => setState(() {
-                              _filter = widget.filters[index];
-                            }),
-                          );
-                        },
-                      ),
-                    ),
-                  ],
+                    )
+            ],
+          ),
+          body: GetBuilder<PublishController>(
+            init: PublishController(),
+            builder: (conroller) {
+              var positionedText = Positioned(
+                right: 15.w,
+                top: 15.w,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(PageRouteBuilder(
+                        opaque: false,
+                        pageBuilder: ((context, animation, secondaryAnimation) {
+                          return TextPage();
+                        })));
+                  },
+                  child: Icon(
+                    Icons.title,
+                    color: Colors.white,
+                    size: 38.w,
+                  ),
                 ),
-        ),
-      ),
+              );
+              return Container(
+                width: double.infinity,
+                height: double.infinity,
+                child: loading
+                    ? widget.loader
+                    : Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10.w),
+                              child: Container(
+                                  width: double.infinity,
+                                  height: double.infinity,
+                                  margin: EdgeInsets.symmetric(
+                                      horizontal: 41.w, vertical: 30.w),
+                                  child: Stack(
+                                    children: [
+                                      positionedBigImage(conroller),
+                                      positionedText,
+                                    ],
+                                  )),
+                            ),
+                          ),
+                          Container(
+                            height: 118.w,
+                            color: Colors.white,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 41.w, vertical: 0.w),
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: widget.filters.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                return InkWell(
+                                  child: Container(
+                                    color: Colors.white,
+                                    padding: const EdgeInsets.all(5.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: <Widget>[
+                                        _buildFilterThumbnail(
+                                            widget.filters[index],
+                                            image,
+                                            filename),
+                                        const SizedBox(
+                                          height: 5.0,
+                                        ),
+                                        Text(
+                                          widget.filters[index].name,
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  onTap: () => setState(() {
+                                    _filter = widget.filters[index];
+                                  }),
+                                );
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+              );
+            },
+          )),
     );
+    return GetBuilder<PublishController>(
+        init: PublishController(),
+        builder: ((controller) {
+          return FloatingDraggableWidget(
+            floatingWidget: GestureDetector(
+              onTap: () {
+                controller.floatHeight =
+                    globalKey.currentContext?.size?.height ?? 1.0;
+                controller.floatWight =
+                    globalKey.currentContext?.size?.width ?? 1.0;
+
+                controller.update();
+                print('height is ${globalKey.currentContext?.size}');
+              },
+              child: Text(
+                key: globalKey,
+                controller.text.value,
+                style: TextStyle(fontSize: 24.sp, color: Colors.white),
+              ),
+            ),
+            floatingWidgetHeight: controller.floatHeight,
+            floatingWidgetWidth: controller.floatWight,
+            dx: 42,
+            dy: 300,
+            deleteWidgetDecoration: const BoxDecoration(
+              // gradient: LinearGradient(
+              //   colors: [Colors.white12, Colors.grey],
+              //   begin: Alignment.topCenter,
+              //   end: Alignment.bottomCenter,
+              //   stops: [.0, 1],
+              // ),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(50),
+                topRight: Radius.circular(50),
+              ),
+            ),
+            deleteWidgetPadding: EdgeInsets.only(bottom: 185.w),
+            deleteWidget: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                border: Border.all(width: 2, color: Colors.black87),
+              ),
+              child: const Icon(Icons.delete_forever, color: Colors.black87),
+            ),
+            onDeleteWidget: () {
+              controller.text.value = '';
+              controller.update();
+              debugPrint('Widget deleted');
+            },
+            mainScreenWidget: safeArea,
+          );
+        }));
   }
 
   _buildFilterThumbnail(
@@ -299,8 +385,8 @@ class _fluttertestPhotoFilterSelectorState
     return imageFile;
   }
 
-  Widget _buildFilteredImage(
-      Filter? filter, imageLib.Image? image, String? filename) {
+  Widget _buildFilteredImage(Filter? filter, imageLib.Image? image,
+      String? filename, PublishController conroller) {
     if (cachedFilters[filter?.name ?? "_"] == null) {
       return FutureBuilder<List<int>>(
         future: compute(applyFilter, <String, dynamic>{
@@ -319,6 +405,7 @@ class _fluttertestPhotoFilterSelectorState
               if (snapshot.hasError)
                 return Center(child: Text('Error: ${snapshot.error}'));
               cachedFilters[filter?.name ?? "_"] = snapshot.data;
+              conroller.imagedata = snapshot.data;
               return widget.circleShape
                   ? SizedBox(
                       height: MediaQuery.of(context).size.width / 3,
@@ -327,7 +414,7 @@ class _fluttertestPhotoFilterSelectorState
                         child: CircleAvatar(
                           radius: MediaQuery.of(context).size.width / 3,
                           backgroundImage: MemoryImage(
-                            snapshot.data as dynamic,
+                            conroller.imagedata as dynamic,
                           ),
                         ),
                       ),
